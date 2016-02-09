d5d27930baa125ede27528c9fe322d324ca62eda
Pierluigi Ruffolo <pieruf@gmail.com>
2016-02-09 17:16:33 +0100

    New input field format. Added stylized input fields for name, password, email, address, phone and ID. Added mozilla compatibility to input fields.

--- assets/js/component-library.js
+++ assets/js/component-library.js
@@ -1,18 +1,21 @@
-var input_fields = 'input[type=text], textarea';
+var input_fields = 'input[type="text"], input[type="password"], textarea';
 
 $(document).ready(function() {
    
    $(input_fields).each(function(index, element) {
-	    if ($(element).val().length > 0) {
-	      $(this).siblings('label, i').addClass('filled');
+	    if ($(element).val().length > 0 || $(this).attr('placeholder') != undefined) {
+	      $(this).siblings('label').addClass('filled');
 	    } else {
-	      $(this).siblings('label, i').removeClass('filled');
+	      $(this).siblings('label').removeClass('filled');
 	    }
 	});
+	
 });
 
 $(document).on('change', input_fields, function () {
-  if($(this).val().length !== 0 || $(this).attr('placeholder') !== undefined) {
-    $(this).siblings('label').addClass('filled');
-  }
+	if($(this).val().length != 0 || $(this).attr('placeholder') != undefined) {
+		$(this).siblings('label').addClass('filled');
+	} else {
+		$(this).siblings('label').removeClass('filled');
+	}
 });
